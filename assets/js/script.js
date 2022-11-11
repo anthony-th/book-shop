6087bf44a4fd54dd3e33a5f427f1a0ad5d84da18
Anthony <yefremov.anthony@gmail.com>
2022-11-12 00:07:59 +0600

    feat: add card-block

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,6 +21,7 @@ rootBlock.appendChild(footer);
 
 header.classList.add('header');
 main.classList.add('main');
+footer.classList.add('footer');
 sectionFirst.classList.add('section');
 sectionFirst.classList.add('section__catalog');
 sectionSecond.classList.add('section');
@@ -33,16 +34,16 @@ header.innerHTML = `
       <nav class="nav">
         <ul class="list">
           <li class="list__item">
-            <a class="link">Sort</a>
+            <a class="link cursor-pointer">Sort</a>
           </li>
           <li class="list__item">
-            <a class="link">Map</a>
+            <a class="link cursor-pointer">Map</a>
           </li>
           <li class="list__item">
-            <a class="link">AppendChild</a>
+            <a class="link cursor-pointer">AppendChild</a>
           </li>
           <li class="list__item">
-            <a class="link">Cart</a>
+            <a class="link cursor-pointer">Cart</a>
           </li>
         </ul>
       </nav>
@@ -50,6 +51,29 @@ header.innerHTML = `
 // main.innerHTML = ``
 // main.innerHTML = `
 //   `   
+sectionFirst.innerHTML = `
+      <div class="card">
+        <div class="card__main-block">
+          <h2 class="card-title">Hey J.A.</h2>
+          <img src="./assets/img/effec_jav.jpg" alt="" class="card-img">
+          <h3 class="card-subtitle">Node.js in Action</h3>
+          <a class="card-link">
+            <h5 class="link-info cursor-pointer user-select">VIEW INFO</h5>
+          </a>
+        </div>
+        <div class="price-block">
+          <p class="card-price">38</p>
+          <a class="card__link cursor-pointer user-select">
+            <img src="./assets/img/icon-shopping-cart.png" class="card-img" alt="">
+          </a>
+        </div>
+      </div>`
+
+footer.innerHTML = `
+      <div class="copyright">
+        Copyright  <a class="link" href="https://github.com/anthony-th">Anthony Th</a>  ©
+      </div>`
+
 
 fetch('./assets/js/books.json') //path to the file with json data
   .then(response => {
