dce34d561f091eb37549df24de7a1f70ebeeb720
Anthony <yefremov.anthony@gmail.com>
2022-11-18 12:33:17 +0600

    refactor: prepare html dom

--- assets/js/delivery.js
+++ assets/js/delivery.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const firstName = document.getElementById('name');
+const lastName = document.getElementById('lastname');
+const date = document.getElementById('date');
+const street = document.getElementById('street');
+const numberHouse = document.getElementById('numberhouse');
+const flatnNumber = document.getElementById('flatnumber');
+
+// if (!firstName.checkValidity()) {
+//   console.log('hey valid');
+// }
+
+// function validate() {
+//   if (firstName.style.length <= 4) {
+//     alert('At least 4 characters!');
+//   }
+// }
