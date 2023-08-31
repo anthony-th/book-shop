1cfb279d29f878c8540f1177bfd8417ff6f3acc1
anthony-th <yefremov.anthony@gmail.com>
2023-08-31 23:23:33 +0300

    feat: add count for cart (Thu, Aug 31, 2023 11:23:33 PM)

--- src/js/header.js
+++ src/js/header.js
@@ -1,35 +1,89 @@
 import cartIconSrc from '../assets/img/icon-shopping-cart.webp';
 
+class Title {
+  constructor(text) {
+    this.element = document.createElement("h1");
+    this.element.className = "title";
+    this.element.innerText = text;
+  }
+
+  getElement() {
+    return this.element;
+  }
+}
+
+class Logo {
+  constructor(titleText) {
+    this.element = document.createElement("div");
+    this.element.className = "logo";
+    this.title = new Title(titleText);
+    this.element.appendChild(this.title.getElement());
+  }
+
+  getElement() {
+    return this.element;
+  }
+}
+
+class Cart {
+  constructor(iconSrc, cartCount) {
+    this.element = document.createElement("div");
+    this.element.className = "cart-block";
+    
+    this.cartIcon = new CartIcon(iconSrc);
+    this.cartCount = new CartCount(cartCount);
+    
+    this.element.appendChild(this.cartIcon.getElement());
+    this.element.appendChild(this.cartCount.getElement());
+  }
+
+  getElement() {
+    return this.element;
+  }
+}
+
+class CartIcon {
+  constructor(iconSrc) {
+    this.element = document.createElement("img");
+    this.element.className = "cart";
+    this.element.src = iconSrc;
+  }
+
+  getElement() {
+    return this.element;
+  }
+}
+
+class CartCount {
+  constructor(count) {
+    this.element = document.createElement("div");
+    this.element.className = "cart-count";
+    this.element.textContent = count;
+  }
+
+  getElement() {
+    return this.element;
+  }
+
+  updateCount(count) {
+    this.element.textContent = count;
+  }
+}
+
 class Header {
   constructor() {
     this.headerElement = document.createElement("header");
     this.headerElement.classList.add("header");
-    this.titleElement = this.createTitle();
-    this.logoElement = this.createLogo();
-    this.cartElement = this.createCart();
-    this.headerElement.appendChild(this.logoElement);
-    this.headerElement.appendChild(this.cartElement);
+
+    this.logo = new Logo("Book-Shop");
+    this.cart = new Cart(cartIconSrc, 0);
+
+    this.headerElement.appendChild(this.logo.getElement());
+    this.headerElement.appendChild(this.cart.getElement());
   }
 
-  createTitle() {
-    const title = document.createElement("h1");
-    title.className = "title";
-    title.innerText = "Book-Shop";
-    return title;
-  }
-
-  createLogo() {
-    const logo = document.createElement("div");
-    logo.className = "logo";
-    logo.appendChild(this.titleElement);
-    return logo;
-  }
-
-  createCart() {
-    const cart = document.createElement("img");
-    cart.src = cartIconSrc;
-    cart.className = "cart";
-    return cart;
+  updateCartCount(count) {
+    this.cart.cartCount.updateCount(count);
   }
 
   getElement() {
